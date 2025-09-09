49ae2e5c0d45932df04d8a0c5eae384f618891a1
Peeradon <76780514+peeradonmoke2002@users.noreply.github.com>
2025-09-09 18:17:18 +0700

    Update ego_vehicle.py

--- carla_ros_bridge/src/carla_ros_bridge/ego_vehicle.py
+++ carla_ros_bridge/src/carla_ros_bridge/ego_vehicle.py
@@ -13,8 +13,8 @@ import math
 import os
 
 import numpy
+import carla
 from carla import VehicleControl, VehicleWheelLocation
-
 from ros_compatibility.qos import QoSProfile, DurabilityPolicy
 
 from carla_ros_bridge.vehicle import Vehicle
@@ -28,8 +28,9 @@ from carla_msgs.msg import (
     CarlaSteeringCurve
 )
 from std_msgs.msg import Bool  # pylint: disable=import-error
-from std_msgs.msg import ColorRGBA  # pylint: disable=import-error
-
+from std_msgs.msg import ColorRGBA
+from autoware_vehicle_msgs.msg import TurnIndicatorsReport, HazardLightsReport
+from autoware_vehicle_msgs.msg import TurnIndicatorsCommand, HazardLightsCommand
 
 class EgoVehicle(Vehicle):
 
@@ -99,6 +100,59 @@ class EgoVehicle(Vehicle):
             self.get_topic_prefix() + "/enable_autopilot",
             self.enable_autopilot_updated,
             qos_profile=10)
+        # --- Autoware status publishers ---
+
+        # --- Autoware status publishers ---
+        self.aw_turn_pub = node.new_publisher(
+            TurnIndicatorsReport, "/vehicle/status/turn_indicators_status", qos_profile=10)
+        self.aw_hazard_pub = node.new_publisher(
+            HazardLightsReport, "/vehicle/status/hazard_lights_status", qos_profile=10)
+
+        self.turn_cmd_sub = node.new_subscription(
+            TurnIndicatorsCommand,
+            "/control/command/turn_indicators_cmd",
+            self._on_turn_cmd,
+            qos_profile=10
+        )
+        self.hazard_cmd_sub = node.new_subscription(
+            HazardLightsCommand,
+            "/control/command/hazard_lights_cmd",
+            self._on_hazard_cmd,
+            qos_profile=10
+        )
+        self._hazard = False
+        self._left = False
+        self._right = False
+        
+    def _apply_lights(self):
+        flags = 0
+        if self._hazard:
+            flags |= carla.VehicleLightState.LeftBlinker | carla.VehicleLightState.RightBlinker
+        else:
+            if self._left:  flags |= carla.VehicleLightState.LeftBlinker
+            if self._right: flags |= carla.VehicleLightState.RightBlinker
+        # optional: make indicators visible
+        if flags:
+            flags |= carla.VehicleLightState.Position | carla.VehicleLightState.LowBeam
+        self.carla_actor.set_light_state(carla.VehicleLightState(flags))
+
+
+    def _on_turn_cmd(self, msg: TurnIndicatorsCommand):
+        # command: 1=DISABLE, 2=ENABLE_LEFT, 3=ENABLE_RIGHT
+        if msg.command == TurnIndicatorsCommand.DISABLE:
+            self._left = self._right = False
+        elif msg.command == TurnIndicatorsCommand.ENABLE_LEFT:
+            self._left, self._right = True, False
+        elif msg.command == TurnIndicatorsCommand.ENABLE_RIGHT:
+            self._left, self._right = False, True
+        self._apply_lights()
+
+
+
+    def _on_hazard_cmd(self, msg: HazardLightsCommand):
+        # command: 1=DISABLE, 2=ENABLE
+        self._hazard = (msg.command == HazardLightsCommand.ENABLE)
+        self._apply_lights()
 
     def get_marker_color(self):
         """
@@ -121,8 +175,8 @@ class EgoVehicle(Vehicle):
 
         :return:
         """
-        vehicle_status = CarlaEgoVehicleStatus(
-            header=self.get_msg_header("map", timestamp=timestamp))
+        header = self.get_msg_header("map", timestamp=timestamp)
+        vehicle_status = CarlaEgoVehicleStatus(header=header)
         vehicle_status.velocity = self.get_vehicle_speed_abs(self.carla_actor)
         vehicle_status.acceleration.linear = self.get_current_ros_accel().linear
         vehicle_status.orientation = self.get_current_ros_pose().orientation
@@ -134,6 +188,25 @@ class EgoVehicle(Vehicle):
         vehicle_status.control.gear = self.carla_actor.get_control().gear
         vehicle_status.control.manual_gear_shift = self.carla_actor.get_control().manual_gear_shift
         self.vehicle_status_publisher.publish(vehicle_status)
+        # --- Publish Autoware turn indicators & hazard lights ---
+        ls = self.carla_actor.get_light_state()
+        left_on  = bool(ls & carla.VehicleLightState.LeftBlinker)
+        right_on = bool(ls & carla.VehicleLightState.RightBlinker)
+
+        ti = TurnIndicatorsReport()
+        ti.stamp = header.stamp
+        if left_on and not right_on:
+          ti.report = TurnIndicatorsReport.ENABLE_LEFT
+        elif right_on and not left_on:
+          ti.report = TurnIndicatorsReport.ENABLE_RIGHT
+        else:
+           ti.report = TurnIndicatorsReport.DISABLE
+        self.aw_turn_pub.publish(ti)
+
+        hz = HazardLightsReport()
+        hz.stamp = header.stamp
+        hz.report = HazardLightsReport.ENABLE if (left_on and right_on) else HazardLightsReport.DISABLE
+        self.aw_hazard_pub.publish(hz)
 
         vehicle_steering = CarlaEgoVehicleSteering(
             header=self.get_msg_header("map", timestamp=timestamp))
@@ -200,6 +273,7 @@ class EgoVehicle(Vehicle):
 
             self.vehicle_info_publisher.publish(vehicle_info)
 
+
     def update(self, frame, timestamp):
         """
         Function (override) to update this object.
@@ -227,6 +301,8 @@ class EgoVehicle(Vehicle):
         self.node.destroy_subscription(self.manual_control_subscriber)
         self.node.destroy_publisher(self.vehicle_status_publisher)
         self.node.destroy_publisher(self.vehicle_info_publisher)
+        self.node.destroy_publisher(self.aw_turn_pub)
+        self.node.destroy_publisher(self.aw_hazard_pub)
         Vehicle.destroy(self)
 
     def control_command_override(self, enable):
